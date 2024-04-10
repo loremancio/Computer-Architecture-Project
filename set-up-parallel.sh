6d4581bdfb8a7244bca7d4bc0504399fe141857e
Giovanni Enrico Loni <65892886+genricoloni@users.noreply.github.com>
2024-04-10 08:48:24 +0200

    some modification on set-up-parallel.sh

--- set-up-parallel.sh
+++ set-up-parallel.sh
@@ -4,15 +4,23 @@
 
 command="AMDuProfCLI"
 argument0="collect --config tbp"
-num_iterazioni=30  # Numero di iterazioni desiderate
-num_exponenti=20  # Numero di esponenti desiderati
-
-cartella="results/par"
-#se la cartella non esiste la creo
-if [ ! -d "$cartella" ]; then
-    mkdir -p $cartella
-fi
+num_iterazioni=5  # Numero di iterazioni desiderate
+num_exponenti=15  # Numero di esponenti desiderati
 
 
 
-#per 
+#per ogni esponte da 0 a 15
+for ((i=0; i<=$num_exponenti; i++)); do
+    dir="results/set-up-par/$i"
+
+    mkdir -p $dir
+    #for each thread
+    for ((j=2; j<=25; j++)); do
+        for ((k=1; k<=$num_iterazioni; k++)); do
+            echo "Esecuzione $k/$num_iterazioni con esponente $(($i)) e thread $j"
+
+            #genero il file di output
+            AMDuProfCLI collect --config threading -o $dir src/par $i $j > /dev/null 2>&1
+        done
+    done
+done
